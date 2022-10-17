6da13cc8547023fe67e73e9c104c54de15e76f2b
Tengya Chu <tengya.chu@es.ey.com>
2022-10-17 18:09:56 +0200

    Lab_SQL_Intro.sql

---
+++ Lab_SQL_Intro.sql
@@ -0,0 +1,15 @@
+use sakila;
+
+select * from actor, film, customer;
+
+select distinct title from film;
+
+select distinct name as language from language;
+
+select count(*) from store;
+
+select count(*) from staff;
+
+select first_name from staff;
+
+select  from film
